f8e40c51a396966ea6d1d3aaeb2783c450ea9e55
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-09-22 17:32:15 +0300

    Update useCategory custom hook to handle search params.

--- src/hooks/useCategory.tsx
+++ src/hooks/useCategory.tsx
@@ -1,29 +1,54 @@
+import { EntityTypes, People } from "@/starWars";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
+import uuid from "react-uuid";
 
 export default function useCategory() {
   const { category } = useParams();
-  const [categoryData, setCategoryData] = useState();
+  const [categoryData, setCategoryData] = useState<People[]>([]);
   const [loading, setLoading] = useState(true);
 
-  const editRow = () => {
-    // setCategoryData((prev) => [...prev, { name: "test", gender: "test" }]);
+  const [searchParams] = useSearchParams();
+  const page = searchParams.get("page") || 1;
+
+  const editRow = (oldPerson: People, updatedPerson: People) => {
+    const updatedPeople = categoryData.map((person) =>
+      person.name === oldPerson.name ? updatedPerson : person
+    );
+    setCategoryData(updatedPeople);
   };
 
-  const deleteRow = (name) => {
-    setCategoryData((prev) => prev.filter((item) => item.name !== name));
+  const addRow = (data: People) => {
+    setCategoryData((prev) => [...prev, data]);
+  };
+
+  const deleteRow = (id?: string) => {
+    setCategoryData((prev) => prev.filter((item) => item.id !== id));
   };
 
   useEffect(() => {
     axios
-      .get(`https://swapi.dev/api/${category}`)
+      .get(`https://swapi.dev/api/${category}/?page=${page}`)
       .then((response) => {
-        console.log(response.data);
-        setCategoryData(response.data.results);
+        console.log(response.data.results);
+
+        const transformedData = response.data.results.map(
+          (item: EntityTypes) => ({
+            id: uuid(),
+            ...item,
+          })
+        );
+        setCategoryData(transformedData);
       })
       .finally(() => setLoading(false));
-  }, [category]);
+  }, [category, page]);
 
-  return { loading, categoryData, deleteRow };
+  return {
+    loading,
+    categoryData,
+    deleteRow,
+    addRow,
+    editRow,
+  };
 }

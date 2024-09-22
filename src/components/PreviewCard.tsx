1a923b2d84b688b6803200f7e3aa06207ae3358d
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-09-22 17:31:17 +0300

    Implement preview card compoennt on search page.

--- src/components/PreviewCard.tsx
+++ src/components/PreviewCard.tsx
@@ -1,9 +1,56 @@
+import {
+  CategoriesNames,
+  Film,
+  People,
+  Planet,
+  Specie,
+  Starship,
+  Vehicle,
+} from "@/starWars";
 import { useNavigate } from "react-router-dom";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
 
-interface PreviewCardProps {
-  category: string;
-  resultsPreview: { name: string }[];
-}
+type PreviewCardProps =
+  | {
+      category: CategoriesNames.films;
+      resultsPreview: Film[];
+    }
+  | {
+      category: CategoriesNames.people;
+      resultsPreview: People[];
+    }
+  | {
+      category: CategoriesNames.planets;
+      resultsPreview: Planet[];
+    }
+  | {
+      category: CategoriesNames.species;
+      resultsPreview: Specie[];
+    }
+  | {
+      category: CategoriesNames.starships;
+      resultsPreview: Starship[];
+    }
+  | {
+      category: CategoriesNames.vehicles;
+      resultsPreview: Vehicle[];
+    };
+
+const getName = (
+  entity: People | Planet | Specie | Starship | Vehicle
+): string => {
+  return entity.name;
+};
+
+const getDisplayField = {
+  [CategoriesNames.films]: (entity: Film): string => entity.title,
+  [CategoriesNames.people]: getName,
+  [CategoriesNames.planets]: getName,
+  [CategoriesNames.species]: getName,
+  [CategoriesNames.starships]: getName,
+  [CategoriesNames.vehicles]: getName,
+};
 
 export default function PreviewCard({
   category,
@@ -16,29 +63,34 @@ export default function PreviewCard({
   };
 
   return (
-    <div className="py-4">
-      <div id="top-section" className="flex items-center justify-between py-1">
-        <h2>{category}</h2>
-        <button
-          className="bg-red-600 px-2 py-1 rounded-md text-color-white cursor-pointer"
-          onClick={handleClick}
+    <Card>
+      <CardContent className="py-4">
+        <div
+          id="top-section"
+          className="flex items-center justify-between py-1"
         >
-          View All
-        </button>
-      </div>
+          <h2>{category}</h2>
+          <Button
+            className="px-2 py-1 rounded-md text-color-white cursor-pointer"
+            onClick={handleClick}
+          >
+            View All
+          </Button>
+        </div>
 
-      <div
-        id="devider"
-        className="w-full h-[2px] bg-gray-500 rounded-md my-2"
-      />
+        <div
+          id="devider"
+          className="w-full h-[2px] bg-gray-500 rounded-md my-2"
+        />
 
-      <ul className="flex flex-col gap-2">
-        {resultsPreview.map((item, index) => (
-          <li key={index} className="text-lg">
-            {item.name}
-          </li>
-        ))}
-      </ul>
-    </div>
+        <ul className="flex flex-col gap-2">
+          {resultsPreview.map((item, index) => (
+            <li key={index} className="text-lg">
+              {getDisplayField[category](item as any)}
+            </li>
+          ))}
+        </ul>
+      </CardContent>
+    </Card>
   );
 }

0c580c9480af7cfcdd3151d6e354faf6dcfabeb0
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-09-25 19:46:12 +0300

    Add ThemeProvider.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import { ThemeProvider } from "./providers/ThemeProvider";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/providers/ThemeProvider.tsx
@@ -0,0 +1,57 @@
+import React, { createContext, useContext, useState } from "react";
+
+export type Theme = "dark" | "light";
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  defaultTheme?: Theme;
+  storageKey?: string;
+};
+
+type ThemeProviderState = {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+};
+
+const initialState: ThemeProviderState = {
+  theme: "dark",
+  setTheme: () => null,
+};
+
+const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
+
+export function ThemeProvider({
+  children,
+  defaultTheme = "dark",
+  ...props
+}: ThemeProviderProps) {
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+  const root = window.document.documentElement;
+
+  const value = {
+    theme,
+    setTheme: (theme: Theme) => {
+      if (root.classList) {
+        root.classList.remove("light", "dark");
+        root.classList.add(theme);
+      }
+      root.classList.add(theme);
+      return setTheme(theme);
+    },
+  };
+
+  return (
+    <ThemeProviderContext.Provider {...props} value={value}>
+      {children}
+    </ThemeProviderContext.Provider>
+  );
+}
+
+export const useTheme = () => {
+  const context = useContext(ThemeProviderContext);
+
+  if (context === undefined)
+    throw new Error("useTheme must be used within a ThemeProvider");
+
+  return context;
+};

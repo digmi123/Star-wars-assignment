730546f89f3baa1d5240d416e3d18880d3b3e65d
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-09-29 01:03:18 +0300

    Add plugin to svgs as component.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react"
+import svgr from "vite-plugin-svgr";
 import { defineConfig } from "vite"
 import path from "path"
  
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

973167ee19f506dfe24c33a83ad3c2b234f4a855
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-09-29 01:04:43 +0300

    Not found in case of wrong url.

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Search from "./pages/Search";
 import Category from "./pages/Category";
+import NotFound from "./pages/NotFound";
+import { categories } from "@/consts";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +12,17 @@ const router = createBrowserRouter([
   {
     path: "/:category",
     element: <Category />,
+    loader: ({ params }) => {
+      if (!categories.includes(params.category)) {
+        throw new Response("Not Found", { status: 404 });
+      }
+      return { category: params.category };
+    },
+    errorElement: <NotFound />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
   },
 ]);
 
